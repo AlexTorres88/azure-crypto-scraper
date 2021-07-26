3a4294dc7669f5af586613b37e7d79f88f6ee359
Alex Torres <dalextor@gmail.com>
2021-07-25 22:17:15 -0500

    Add market cap to bot response

--- CryptoBot/Scraper.py
+++ CryptoBot/Scraper.py
@@ -22,25 +22,39 @@ def get_prices():
         r = list(row.children)
         name_col = r[2]
         # Find the p tag that contains the name of the coin
-        p = name_col.find('p', class_="coin-item-symbol")
+        name_p = name_col.find('p', class_="coin-item-symbol")
         name = ""
-        if p:
-            name = p.get_text()  
+        if name_p:
+            name = name_p.get_text()  
+
+        # Skip row if the we don't want that crypto
+        if name not in coins:
+            continue
 
         price_col = r[3]
         # Find the a tag that contains the price 
-        a = price_col.find('a')
+        price_a = price_col.find('a')
 
         price = 0
-        if a:
-            price = a.get_text()
+        if price_a:
+            price = price_a.get_text()
+        
+        market_cap_col = r[6]
+        market_cap_p = market_cap_col.find('p')
 
+        market_cap = ""
+        if market_cap_p:
+            market_cap_p_children = list(market_cap_p.children)
+            market_cap = market_cap_p_children[1].get_text()
+
+        
         coin = {}
-        if name in coins: 
-            coin["name"] = name
-            coin["price"] = price
-            coin["timestamp"] = datetime.utcnow()
-            res.append(coin)
-            ind += 1
+        coin["name"] = name
+        coin["price"] = price
+        coin["timestamp"] = datetime.utcnow()
+        coin["market_cap"] = market_cap
+        res.append(coin)
+        ind += 1
+
     # Convert any type you don't recognize to string (Datetime in this case)
     return  json.dumps(res, indent=4, sort_keys=True, default=str)
